7ddb4412bc044e52b0efb8855a4d75e36503532b
Fawzan <fawzaaninsani@gmail.com>
2023-11-26 12:35:36 +0700

    update pa

--- Proyek_akhir.py
+++ Proyek_akhir.py
@@ -15,29 +15,36 @@ paket = [paket1 + "(35000)", paket2 + "(60000)" , paket3 + "(90000)", paket4 + "
 
 # Membuat fungsi isi pulsa
 def isi_pulsa(pulsa) :
-    global Pulsa
     if pulsa == 1 :
-        if MU_pay - 20000 > 0:
+        if uang["MU_pay"] - 20000 >= 0:
             print(f"Pulsa {menu_pulsa[0]} berhasil diisi")
-            return Pulsa + 20000 
+            hitung_uang = lambda uang : uang - 20000
+            uang["MU_pay"] = hitung_uang(uang["MU_pay"])
+            menu_utama["Pulsa"] = menu_utama["Pulsa"] + 20000
         else :
             print("Saldo MU pay anda tidak cukup, silahkan isi dulu")
     elif pulsa == 2 :
-        if MU_pay - 50000 > 0:
+        if uang["MU_pay"] - 50000 >= 0:
             print(f"Pulsa {menu_pulsa[1]} berhasil diisi")
-            return Pulsa + 50000
+            hitung_uang = lambda uang : uang - 50000
+            uang["MU_pay"] = hitung_uang(uang["MU_pay"])
+            menu_utama["Pulsa"] = menu_utama["Pulsa"] + 50000
         else :
             print("Saldo MU pay anda tidak cukup, silahkan isi dulu")
     elif pulsa == 3 :
-        if MU_pay - 100000 > 0 :
+        if uang["MU_pay"] - 100000 >= 0 :
             print(f"Pulsa {menu_pulsa[2]} berhasil diisi")
-            return Pulsa + 100000
+            hitung_uang = lambda uang : uang - 100000
+            uang["MU_pay"] = hitung_uang(uang["MU_pay"])
+            menu_utama["Pulsa"] = menu_utama["Pulsa"] + 100000
         else :
             print("Saldo MU pay anda tidak cukup, silahkan isi dulu")
     elif pulsa == 4 : 
-        if MU_pay - 200000 > 0 :
+        if uang["MU_pay"] - 200000 >= 0 :
             print(f"Pulsa {menu_pulsa[3]} berhasil diisi")
-            return Pulsa + 200.000
+            hitung_uang = lambda uang : uang - 200000
+            uang["MU_pay"] = hitung_uang(uang["MU_pay"])
+            menu_utama["Pulsa"] = menu_utama["Pulsa"] + 200000
         else :
             print("Saldo MU pay anda tidak cukup, silahkan isi dulu")
     else :
@@ -45,29 +52,31 @@ def isi_pulsa(pulsa) :
 
 # Membuat fungsi isi paket data
 def isi_paket_data(paket) :
-    global Paket_Data
     if paket == 1 :
-        if MU_pay - 35000 > 0:
+        if uang["MU_pay"] - 35000 >= 0:
             print(f"Paket Data {paket1} berhasil diisi")
-            return Paket_Data.append(paket1)
+            uang["MU_pay"] = uang["MU_pay"] - 35000
+            menu_utama["Paket_Data"] = "5GB"
         else :
             print("Saldo MU pay anda tidak cukup, silahkan isi dulu")
     elif paket == 2 :
-        if MU_pay - 60000 > 0:
+        if uang["MU_pay"] - 60000 >= 0:
             print(f"Paket Data {paket2} berhasil diisi")
-            return Paket_Data.append(paket2)
+            uang["MU_pay"] = uang["MU_pay"] - 60000
+            menu_utama["Paket_Data"] = "10GB"
         else :
             print("Saldo MU pay anda tidak cukup, silahkan isi dulu")
     elif paket == 3 :
-        if MU_pay - 90000 > 0:
-            print(f"Paket Data {paket3} berhasil diisi")
-            return Paket_Data.append(paket3)
+        if uang["MU_pay"] - 90000 >= 0:
+            uang["MU_pay"] = uang["MU_pay"] - 90000
+            menu_utama["Paket_Data"] = "30GB"
         else :
             print("Saldo MU pay anda tidak cukup, silahkan isi dulu")
     elif paket == 4 :
-        if MU_pay - 120000 > 0:
+        if uang["MU_pay"] - 120000 >= 0:
             print(f"Paket Data {paket4} berhasil diisi")
-            return Paket_Data.append(paket4)
+            uang["MU_pay"] = uang["MU_pay"] - 120000
+            menu_utama["Paket_Data"] = "40GB"
         else :
             print("Saldo MU pay anda tidak cukup, silahkan isi dulu")
     else :
@@ -79,24 +88,22 @@ print()
 
 # Membuat Variable untuk menyimpan saldo MU pay
 saldo = int(input("Masukkan saldo MU pay yang anda miliki: "))
-MU_pay = saldo
-
+menu_utama = {"Pulsa" : 0 , "Paket_Data" : [] }
+uang = { "MU_pay" : saldo}
 # Membuat Looping dengan While True
 while True :
-    # Membuat Variable untuk menyimpan setiap menu
-    Pulsa = 0
-    Paket_Data = []
+    print(menu_utama)
     print("Sisa Pulsa, Paket data, dan Mu pay anda hari ini:")
-    print("Pulsa =", Pulsa)
-    print("Paket Data = ", *Paket_Data, sep="")
-    print("MU pay =", MU_pay)
+    print("Pulsa =", menu_utama["Pulsa"])
+    print("Paket Data = ", *menu_utama["Paket_Data"], sep="")
+    print("MU pay =", uang["MU_pay"])
 
     print()
 
     # Membuat Menu Aktivitas
     print("Menu di My MU:")
-    menu = ["Membeli Pulsa", "Membeli Paket Data"]
-    for nomor, isi in enumerate(menu, start=1):
+    menu_mymu = ["Membeli Pulsa", "Membeli Paket Data"]
+    for nomor, isi in enumerate(menu_mymu, start=1):
         print(f"{nomor}. {isi}")
 
     # Menginput Aktivitas
@@ -107,6 +114,7 @@ while True :
         for nomor, isi in enumerate(menu_pulsa, start=1):
             print(f"{nomor}. {isi}")
         tanya_pulsa = int(input("Menu pulsa mana yang ingin anda beli? "))
+        print(menu_utama)
         Pulsa = isi_pulsa(tanya_pulsa)
     elif aktivitas == 2 :
         for nomor, isi in enumerate(paket, start=1):
@@ -120,18 +128,18 @@ while True :
 
     #Menampikan sisa saldo 
     print("Sisa Pulsa, Paket data, dan MU pay anda hari ini:")
-    print("Pulsa =", Pulsa)
-    print("Paket Data = ", *Paket_Data, sep="")
-    print("MU pay =", MU_pay)
+    print("Pulsa =", menu_utama["Pulsa"])
+    print("Paket Data = ", *menu_utama["Paket_Data"], sep="")
+    print("MU pay =", uang["MU_pay"])
     print()
 
     tanya = input("Apakah anda ingin menggunakan layanan My MU lagi (y/t)? ")
     if tanya != "y" :
         #Menampilkan Pulsa, Paket Data, dan MU pay setelah transaksi
         print("Sisa Pulsa, Paket data, dan MU pay anda hari ini:")
-        print("Pulsa =", Pulsa)
-        print("Paket Data = ", *Paket_Data, sep="")
-        print("MU pay =", MU_pay)
+        print("Pulsa =", menu_utama["Pulsa"])
+        print("Paket Data = ", *menu_utama["Paket_Data"], sep="")
+        print("MU pay =", uang["MU_pay"])
         print()
         print("Terima Kasih telah menggunakan layanan My MU🙏")
         print("Semoga hari mu menyenangkan dan MU menang terus")
